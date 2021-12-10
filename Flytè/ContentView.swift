d7692f766eaf279d94b408211942f59e5dcffdc7
Mattia Fochesato <36085165+MattiaFochesato@users.noreply.github.com>
2021-12-10 10:34:24 +0100

    Initial Commit

---
+++ Flytè/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flytè
+//
+//  Created by Mattia Fochesato on 10/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flytè/Flyte_App.swift
@@ -0,0 +1,17 @@
+//
+//  Flyte_App.swift
+//  Flytè
+//
+//  Created by Mattia Fochesato on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Flyte_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
